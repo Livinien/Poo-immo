bf1290730b9429e9d372ab2ab247a395716581e6
Flavien <livinien@hotmail.fr>
2023-01-05 14:51:16 +0100

    Finalisation de l'inscription et modifications de la page connexion

--- Front/connexion_front.php
+++ Front/connexion_front.php
@@ -17,17 +17,18 @@
 
 <body>
 
-    <div class="formulaire d-flex justify-content-center">
-        <form class="form text-center">
+    <div class="formulaire-connexion d-flex justify-content-center">
+        <form class="form-connexion text-center">
             <h1 class="title-connexion text-center">Connexion</h1>
 
             <div class="mt-5 row d-flex justify-content-center">
-                <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control " id="exampleInputEmail1" aria-describedby="emailHelp">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control">
             </div>
+
             <div class="mt-5 row row d-flex justify-content-center">
-                <label for="exampleInputPassword1" class="form-label">Mot de Passe</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
+                <label for="password" class="form-label">Mot de Passe</label>
+                <input type="password" class="form-control">
             </div>
 
 
@@ -35,7 +36,8 @@
                 <button type="submit" class="button-connexion btn btn-success">Connexion</button><br>
 
                 <div class="mt-4 text-center">
-                    <span class="link-inscription">Pas encore de compte ? <a href="#">S'inscrire</a></span>
+                    <span class="link-inscription">Pas encore de compte ? <a
+                            href="inscription_front.php">S'inscrire</a></span>
                 </div>
             </div>
         </form>
---
+++ Front/inscription_front.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="./style.css">
+
+    <title>IMMOBELLO - Inscription</title>
+
+
+</head>
+
+<body>
+
+    <div class="formulaire-inscription d-flex justify-content-center">
+        <form class="form-inscription text-center">
+            <h1 class="title-inscription text-center">Inscription</h1>
+
+            <div class="mt-4 row d-flex justify-content-center">
+                <label for="prénom" class="form-label">Prénom</label>
+                <input type="text" class="form-control">
+            </div>
+
+            <div class="mt-4 row d-flex justify-content-center">
+                <label for="nom" class="form-label">Nom</label>
+                <input type="text" class="form-control">
+            </div>
+
+            <div class="mt-4 row d-flex justify-content-center">
+                <label for="email" class="form-label">Email</label>
+                <input type="email" class="form-control">
+            </div>
+
+            <div class=" mt-4 row row d-flex justify-content-center">
+                <label for="password1" class="form-label">Mot de Passe</label>
+                <input type="password" class="form-control">
+            </div>
+
+            <div class="mt-4 row row d-flex justify-content-center">
+                <label for="password2" class="form-label">Confirmer votre mot de passe</label>
+                <input type="password" class="form-control">
+            </div>
+
+
+            <div class="m-4 text-center">
+                <button type="submit" class="button-inscription btn btn-success">S'inscrire</button><br>
+
+                <div class="mt-4 text-center">
+                    <span class="link-connexion">Vous avez déjà un compte ? <a href="connexion_front.php">Se
+                            Connecter</a></span>
+                </div>
+            </div>
+        </form>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
