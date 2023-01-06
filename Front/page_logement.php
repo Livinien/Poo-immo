fa5b3676bad4eda6b15ebaaf31dcf797013a801e
djebbar1 <djebbar.amaoui.simplon@gmail.com>
2023-01-06 11:55:42 +0100

    RECUP SASS

--- Front/page_logement.php
+++ Front/page_logement.php
@@ -11,7 +11,7 @@
     <link rel="stylesheet" href="./style.css">
 
 
-    <title>Immobello - Accueil</title>
+    <title>Immobello - Page_Logement</title>
 
 </head>
 
@@ -98,28 +98,50 @@
 
 
     <!-- CARTE DU LOGEMENT -->
-
-    <div class="card-body">
+   
+    <div class="card-body w-25">
         <h2 class="card-title">Villa de Luxe</h2>
             <h4 class="card-subtitle mt-3">Surface 3500 m2 au total</h4>
-                <h4 class="card-subtitle "> prix: 1 000 000 €</h4>
+                <h4 class="card-subtitle mt-3 mb-3"> prix: 1 000 000 €</h4>
     </div>
 
-    <div class="card-body">
+    <div class="card-body w-25">
         <h2 class="card-title">Caractéristiques</h2>
             <h4 class="card-subtitle mt-3">Type de logement: Villa, Maison</h4>
-                <h4 class="card-subtitle"> Nombre de Pièces: T6 </h4>
-                <h4 class="card-subtitle"> Surface: 200 m2 surface intérieur avec un jardin de 3000 m2 + piscine. </h4>
-                <h4 class="card-subtitle"> Localisation: Nice, FRANCE </h4>
+                <h4 class="card-subtitle mt-3 mb-3"> Nombre de Pièces: T6 </h4>
+                <h4 class="card-subtitle mt-3 mb-3"> Surface: 200 m2 surface intérieur avec un jardin de 3000 m2 + piscine. </h4>
+                <h4 class="card-subtitle mt-3 mb-3"> Localisation: Nice, FRANCE </h4>
     </div>
-    <div class="card-body">
+    <div class="card-body w-25">
         <h2 class="card-title">Description</h2>
             <h4 class="card-subtitle mt-3">Villa face à la mer avec piscine, se situant sur les hauteurs. Coin tranquille sans vis-à-vis situé dans la résidence huppé de Nice.
             </h4>
-                <h4 class="card-subtitle"> A proximité des commerces, de la plage, du centre-ville, etc... </h4>
+                <h4 class="card-subtitle mt-3 mb-3"> A proximité des commerces, de la plage, du centre-ville, etc... </h4>
 
     </div>
 
+    <!--  contact  -->
+
+    <div class="card_contact w-25 p-3 bg-red">
+    <div class="card-header">
+        Contacter le vendeur
+    </div>
+    <div class="card-body">
+    <div class="mb-3">
+        <label for="exampleFormControlInput1" class="form-label">Nom</label>
+        <input type="name" class="form-control" id="exampleFormControlInput1" placeholder="Nom">
+        </div>
+        <div class="mb-3">
+        <label for="exampleFormControlInput2" class="form-label">Téléphone</label>
+        <input type="tel" class="form-control" id="exampleFormControlInput2" placeholder="Téléphone">
+        </div>
+        <div class="mb-3">
+        <label for="exampleFormControlInput3" class="form-label">Email address</label>
+        <input type="email" class="form-control" id="exampleFormControlInput3" placeholder="name@example.com">
+        </div>
+        <button class="btn btn-success ms-3 contact" type="submit">Contacter l'agence pour plus d'information</button>
+    </div>
+</div>
 
     <div class=" d-flex flex-column justify-content-evenly bg-dark bg-gradient text-light p-4">
         <div class="d-flex justify-content-between">
